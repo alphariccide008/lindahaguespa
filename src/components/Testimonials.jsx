bce7d5eaf8b68d5e58b36c46dfc0a58cb0e51755
alpha <godspowerlawrence008@gmail.com>
2025-02-18 06:31:29 -0500

    third commit

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -11,7 +11,7 @@ const Testimonials = () => {
 				<div className="elementor-element elementor-element-b672598 elementor-widget__width-initial elementor-view-default elementor-widget elementor-widget-icon" data-id="b672598" data-element_type="widget" data-widget_type="icon.default">
 				<div className="elementor-widget-container">
 							<div className="elementor-icon-wrapper">
-			<a className="elementor-icon" href="https://msng.link/o?%20@yourfavoritegirlyyyy=tg">
+			<a className="elementor-icon" href="https://t.me/asap0077">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48px" height="48px"><path fill="#29b6f6" d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"></path><path fill="#fff" d="M33.95,15l-3.746,19.126c0,0-0.161,0.874-1.245,0.874c-0.576,0-0.873-0.274-0.873-0.274l-8.114-6.733 l-3.97-2.001l-5.095-1.355c0,0-0.907-0.262-0.907-1.012c0-0.625,0.933-0.923,0.933-0.923l21.316-8.468 c-0.001-0.001,0.651-0.235,1.126-0.234C33.667,14,34,14.125,34,14.5C34,14.75,33.95,15,33.95,15z"></path><path fill="#b0bec5" d="M23,30.505l-3.426,3.374c0,0-0.149,0.115-0.348,0.12c-0.069,0.002-0.143-0.009-0.219-0.043 l0.964-5.965L23,30.505z"></path><path fill="#cfd8dc" d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z"></path></svg>			</a>
 		</div>
 						</div>
@@ -19,7 +19,7 @@ const Testimonials = () => {
 				<div className="elementor-element elementor-element-1b4f76d elementor-view-default elementor-widget elementor-widget-icon" data-id="1b4f76d" data-element_type="widget" data-widget_type="icon.default">
 				<div className="elementor-widget-container">
 							<div className="elementor-icon-wrapper">
-			<a className="elementor-icon" href="https://signal.me/#p/%2B12244549118">
+			<a className="elementor-icon" href="https://signal.me/#eu/aG4ii6BJVXT33nplW0tuWKPeFcpgfc08u-V2NO3HntrsKANsBdNBe46UUhp4SX9U">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48px" height="48px"><linearGradient id="nYMvCsU2aI2MBCA4ho~Qna" x1="24" x2="24" y1="333.541" y2="293.009" gradientTransform="matrix(1 0 0 -1 0 340)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#2aa4f4"></stop><stop offset="1" stop-color="#007ad9"></stop></linearGradient><path fill="url(#nYMvCsU2aI2MBCA4ho~Qna)" d="M35,42H13c-3.866,0-7-3.134-7-7V13c0-3.866,3.134-7,7-7h22c3.866,0,7,3.134,7,7v22 C42,38.866,38.866,42,35,42z"></path><path fill="#fff" d="M11.946,36.554c-0.132,0-0.259-0.052-0.354-0.146c-0.109-0.109-0.162-0.263-0.143-0.416l0.25-1.984 l0.992,0.125l-0.169,1.345l1.345-0.169l0.125,0.992l-1.984,0.25C11.988,36.552,11.967,36.554,11.946,36.554z"></path><path fill="#fff" d="M24,37c-0.799,0-1.603-0.075-2.388-0.224l0.186-0.982c1.247,0.235,2.541,0.267,3.796,0.101 l0.132,0.991C25.156,36.961,24.576,37,24,37z M27.743,36.45l-0.288-0.958c1.218-0.366,2.367-0.924,3.415-1.659l0.574,0.818 C30.309,35.448,29.063,36.053,27.743,36.45z M19.621,36.234c-0.402-0.145-0.8-0.311-1.185-0.494l-2.426,0.306l-0.125-0.992 l2.573-0.324c0.097-0.011,0.195,0.004,0.283,0.047c0.394,0.193,0.804,0.367,1.219,0.517L19.621,36.234z M33.036,33.339 l-0.695-0.719c0.919-0.89,1.687-1.912,2.28-3.038l0.885,0.467C34.862,31.269,34.031,32.376,33.036,33.339z M12.941,32.161 l-0.992-0.125l0.312-2.472c-0.176-0.37-0.337-0.753-0.478-1.14l0.939-0.343c0.146,0.401,0.315,0.797,0.502,1.178 c0.043,0.088,0.06,0.186,0.047,0.283L12.941,32.161z M36.32,28.152l-0.947-0.32c0.407-1.204,0.618-2.464,0.627-3.745l1,0.007 C36.99,25.481,36.761,26.847,36.32,28.152z M11.232,26.435C11.078,25.635,11,24.815,11,24c0-0.563,0.037-1.128,0.108-1.679 L12.1,22.45C12.034,22.958,12,23.48,12,24c0,0.752,0.072,1.508,0.214,2.246L11.232,26.435z M35.864,22.187 c-0.192-1.26-0.583-2.476-1.162-3.613l0.891-0.454c0.628,1.233,1.052,2.551,1.26,3.917L35.864,22.187z M12.494,20.587l-0.959-0.285 c0.393-1.322,0.994-2.569,1.785-3.708l0.821,0.571C13.411,18.216,12.856,19.367,12.494,20.587z M33.706,16.95 c-0.752-1.032-1.658-1.934-2.692-2.681l0.586-0.811c1.119,0.809,2.1,1.786,2.914,2.903L33.706,16.95z M15.35,15.691l-0.722-0.692 c0.958-0.998,2.062-1.833,3.28-2.482l0.471,0.883C17.253,13.999,16.234,14.77,15.35,15.691z M29.385,13.277 c-1.14-0.575-2.357-0.961-3.619-1.148l0.146-0.989c1.368,0.203,2.688,0.622,3.922,1.245L29.385,13.277z M20.125,12.643L19.8,11.696 c1.304-0.446,2.668-0.68,4.055-0.695l0.011,1C22.586,12.015,21.327,12.231,20.125,12.643z"></path><g><path d="M24,14c5.523,0,10,4.477,10,10s-4.477,10-10,10c-1.901,0-3.671-0.54-5.185-1.461l-3.906,0.552l0.552-3.906 C14.54,27.671,14,25.901,14,24C14,18.477,18.477,14,24,14 M24,13c-6.065,0-11,4.935-11,11c0,1.884,0.491,3.739,1.422,5.39 l-0.503,3.561l-0.186,1.316l1.316-0.186l3.561-0.503C20.261,34.509,22.116,35,24,35c6.065,0,11-4.935,11-11S30.065,13,24,13L24,13 z" opacity=".05"></path><path d="M24,14c5.523,0,10,4.477,10,10s-4.477,10-10,10c-1.901,0-3.671-0.54-5.185-1.461l-3.906,0.552l0.552-3.906 C14.54,27.671,14,25.901,14,24C14,18.477,18.477,14,24,14 M24,13.5c-5.79,0-10.5,4.71-10.5,10.5c0,1.853,0.498,3.678,1.441,5.289 l-0.527,3.732l-0.093,0.658l0.658-0.093l3.732-0.527C20.322,34.002,22.147,34.5,24,34.5c5.79,0,10.5-4.71,10.5-10.5 S29.79,13.5,24,13.5L24,13.5z" opacity=".07"></path><path fill="#fff" d="M24,14c-5.523,0-10,4.477-10,10c0,1.901,0.54,3.671,1.461,5.185l-0.552,3.906l3.906-0.552 C20.329,33.46,22.099,34,24,34c5.523,0,10-4.477,10-10S29.523,14,24,14z"></path></g></svg>			</a>
 		</div>
 						</div>
@@ -35,7 +35,7 @@ const Testimonials = () => {
 				<div className="elementor-element elementor-element-955cb67 elementor-view-default elementor-widget elementor-widget-icon" data-id="955cb67" data-element_type="widget" data-widget_type="icon.default">
 				<div className="elementor-widget-container">
 							<div className="elementor-icon-wrapper">
-			<a className="elementor-icon" href="http://wa.link/tb3or0">
+			<a className="elementor-icon" href="https://wa.me/13143237087">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48px" height="48px" clip-rule="evenodd"><path fill="#fff" d="M4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98c-0.001,0,0,0,0,0h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303z"></path><path fill="#fff" d="M4.868,43.803c-0.132,0-0.26-0.052-0.355-0.148c-0.125-0.127-0.174-0.312-0.127-0.483l2.639-9.636c-1.636-2.906-2.499-6.206-2.497-9.556C4.532,13.238,13.273,4.5,24.014,4.5c5.21,0.002,10.105,2.031,13.784,5.713c3.679,3.683,5.704,8.577,5.702,13.781c-0.004,10.741-8.746,19.48-19.486,19.48c-3.189-0.001-6.344-0.788-9.144-2.277l-9.875,2.589C4.953,43.798,4.911,43.803,4.868,43.803z"></path><path fill="#cfd8dc" d="M24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,4C24.014,4,24.014,4,24.014,4C12.998,4,4.032,12.962,4.027,23.979c-0.001,3.367,0.849,6.685,2.461,9.622l-2.585,9.439c-0.094,0.345,0.002,0.713,0.254,0.967c0.19,0.192,0.447,0.297,0.711,0.297c0.085,0,0.17-0.011,0.254-0.033l9.687-2.54c2.828,1.468,5.998,2.243,9.197,2.244c11.024,0,19.99-8.963,19.995-19.98c0.002-5.339-2.075-10.359-5.848-14.135C34.378,6.083,29.357,4.002,24.014,4L24.014,4z"></path><path fill="#40c351" d="M35.176,12.832c-2.98-2.982-6.941-4.625-11.157-4.626c-8.704,0-15.783,7.076-15.787,15.774c-0.001,2.981,0.833,5.883,2.413,8.396l0.376,0.597l-1.595,5.821l5.973-1.566l0.577,0.342c2.422,1.438,5.2,2.198,8.032,2.199h0.006c8.698,0,15.777-7.077,15.78-15.776C39.795,19.778,38.156,15.814,35.176,12.832z"></path><path fill="#fff" fill-rule="evenodd" d="M19.268,16.045c-0.355-0.79-0.729-0.806-1.068-0.82c-0.277-0.012-0.593-0.011-0.909-0.011c-0.316,0-0.83,0.119-1.265,0.594c-0.435,0.475-1.661,1.622-1.661,3.956c0,2.334,1.7,4.59,1.937,4.906c0.237,0.316,3.282,5.259,8.104,7.161c4.007,1.58,4.823,1.266,5.693,1.187c0.87-0.079,2.807-1.147,3.202-2.255c0.395-1.108,0.395-2.057,0.277-2.255c-0.119-0.198-0.435-0.316-0.909-0.554s-2.807-1.385-3.242-1.543c-0.435-0.158-0.751-0.237-1.068,0.238c-0.316,0.474-1.225,1.543-1.502,1.859c-0.277,0.317-0.554,0.357-1.028,0.119c-0.474-0.238-2.002-0.738-3.815-2.354c-1.41-1.257-2.362-2.81-2.639-3.285c-0.277-0.474-0.03-0.731,0.208-0.968c0.213-0.213,0.474-0.554,0.712-0.831c0.237-0.277,0.316-0.475,0.474-0.791c0.158-0.317,0.079-0.594-0.04-0.831C20.612,19.329,19.69,16.983,19.268,16.045z" clip-rule="evenodd"></path></svg>			</a>
 		</div>
 						</div>
@@ -59,26 +59,26 @@ const Testimonials = () => {
 			
 				</div>
 
-        <a
-          href="#"
+        <Link
+          to='/contact'
           className="bg-[#000000] w-36 mx-1 md:text-[15px] my-5  flex rounded-lg text-sm text-white p-3  hover:text-[#697dbb] "
         >
           Book A Section 
-        </a>
+        </Link>
           </div>
             <div className="flex-col md:w-1/2 px-2">
             <div className="rounded-xl bg-white py-[4%] px-[4%] my-3">
                     <h1 className="py-3 md:text-[50px] text-[25px] font-semibold">Nuru Massage</h1>
                     <p className="py-4">A tailored full body massage including abdomen, face and feet. This massage will work your body from head to toe and leave you feeling at peace with the world whilst internally your body will de stress and eliminate toxins in the blood, muscle, skin and mind.</p>
                     <div className=" my-3">
-                        <Link to='' className="text-center py-3 px-5 hover:text-[#4b48e3] bg-black rounded-lg text-white">Book Now</Link>
+                        <Link to='/contact' className="text-center py-3 px-5 hover:text-[#4b48e3] bg-black rounded-lg text-white">Book Now</Link>
                     </div>
                 </div>
                 <div className="rounded-xl bg-white py-[4%] px-[4%] my-3">
                     <h1 className="py-3 md:text-[50px] text-[25px] font-semibold">Hot Stone Massage</h1>
                     <p className="py-4">During a hot stone massage, I use smooth, heated stones placed on your body to provide a deep, soothing warmth that relaxes tight muscles, eases stress, and promotes a profound sense of relaxation</p>
                     <div className=" my-3">
-                        <Link to='' className="text-center py-3 px-5 hover:text-[#4b48e3] bg-black rounded-lg text-white">Book Now</Link>
+                        <Link to='/contact' className="text-center py-3 px-5 hover:text-[#4b48e3] bg-black rounded-lg text-white">Book Now</Link>
                     </div>
                 </div>
             </div>
